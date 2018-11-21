0b0ea10eaad60ee280f9f8fbd745a42fda37ee67
lowkey <mephistopheles@onlinehome.de>
2018-11-21 15:02:31 +0100

    added type trait

--- include/sf2/sf2.hpp
+++ include/sf2/sf2.hpp
@@ -30,38 +30,54 @@ namespace sf2 {
 	using JsonSerializer   = Serializer<format::Json_writer>;
 	using JsonDeserializer = Deserializer<format::Json_reader>;
 
-	template<typename T>
-	inline void serialize_json(std::ostream& stream, const T& v) {
+	template <typename T>
+	constexpr auto is_json_serializable =
+	        is_annotated_struct<T>::value || details::has_save<format::Json_writer, T>::value;
+	template <typename T>
+	constexpr auto is_json_deserializable =
+	        is_annotated_struct<T>::value || details::has_load<format::Json_reader, T>::value;
+
+
+	template <typename T>
+	inline void serialize_json(std::ostream& stream, const T& v)
+	{
 		JsonSerializer{format::Json_writer{stream}}.write(v);
 	}
-	template<typename... Members>
-	inline void serialize_json_virtual(std::ostream& stream, Members&&... m) {
+	template <typename... Members>
+	inline void serialize_json_virtual(std::ostream& stream, Members&&... m)
+	{
 		JsonSerializer{format::Json_writer{stream}}.write_virtual(std::forward<Members>(m)...);
 	}
 
-	template<typename T>
-	inline auto deserialize_json(std::istream& stream) -> T {
+	template <typename T>
+	inline auto deserialize_json(std::istream& stream) -> T
+	{
 		auto v = T();
 		JsonDeserializer{format::Json_reader{stream}}.read(v);
 		return v;
 	}
-	template<typename T>
-	inline void deserialize_json(std::istream& stream, T& v) {
+	template <typename T>
+	inline void deserialize_json(std::istream& stream, T& v)
+	{
 		JsonDeserializer{format::Json_reader{stream}}.read(v);
 	}
-	template<typename T>
-	inline void deserialize_json(std::istream& stream, format::Error_handler on_error, T& v) {
+	template <typename T>
+	inline void deserialize_json(std::istream& stream, format::Error_handler on_error, T& v)
+	{
 		JsonDeserializer{format::Json_reader{stream, on_error}, on_error}.read(v);
 	}
-	template<typename... Members>
-	inline void deserialize_json_virtual(std::istream& stream, Members&&... m) {
+	template <typename... Members>
+	inline void deserialize_json_virtual(std::istream& stream, Members&&... m)
+	{
 		JsonDeserializer{format::Json_reader{stream}}.read_virtual(std::forward<Members>(m)...);
 	}
-	template<typename... Members>
-	inline void deserialize_json_virtual(std::istream& stream, format::Error_handler on_error, Members&&... m) {
-		JsonDeserializer{format::Json_reader{stream, on_error}, on_error}.read_virtual(std::forward<Members>(m)...);
+	template <typename... Members>
+	inline void deserialize_json_virtual(std::istream& stream, format::Error_handler on_error, Members&&... m)
+	{
+		JsonDeserializer{format::Json_reader{stream, on_error}, on_error}.read_virtual(
+		        std::forward<Members>(m)...);
 	}
 
-}
+} // namespace sf2
 
 #endif
